a3dc51339e762c8bb176f815609fa87e4f11ab8a
piptouque <piptouque@protonmail.com>
2021-10-11 18:01:22 +0200

    [+]: update changes.

--- helper_plot.py
+++ helper_plot.py
@@ -8,24 +8,24 @@ from matplotlib.patches import Ellipse
 
 def hdr_plot_style():
     plt.style.use('dark_background')
-    mpl.rcParams.update({'font.size': 18, 'lines.linewidth': 3, 'lines.markersize': 15})
+    # mpl.rcParams.update({'font.size': 18, 'lines.linewidth': 3, 'lines.markersize': 15})
     # avoid type 3 (i.e. bitmap) fonts in figures
-    mpl.rcParams['ps.useafm'] = True
-    mpl.rcParams['pdf.use14corefonts'] = True
-    mpl.rcParams['text.usetex'] = False
-    mpl.rcParams['font.family'] = 'sans-serif'
-    mpl.rcParams['font.sans-serif'] = 'Courier New'
-    #mpl.rcParams['text.hinting'] = False
+    # mpl.rcParams['ps.useafm'] = True
+    # mpl.rcParams['pdf.use14corefonts'] = True
+    # mpl.rcParams['text.usetex'] = False
+    # mpl.rcParams['font.family'] = 'sans-serif'
+    # mpl.rcParams['font.sans-serif'] = 'Courier New'
+    # mpl.rcParams['text.hinting'] = False
     # Set colors cycle
     colors = mpl.cycler('color', ['#3388BB', '#EE6666', '#9988DD', '#EECC55', '#88BB44', '#FFBBBB'])
     #plt.rc('figure', facecolor='#00000000', edgecolor='black')
     #plt.rc('axes', facecolor='#FFFFFF88', edgecolor='white', axisbelow=True, grid=True, prop_cycle=colors)
-    plt.rc('legend', facecolor='#666666EE', edgecolor='white', fontsize=16)
-    plt.rc('grid', color='white', linestyle='solid')
-    plt.rc('text', color='white')
-    plt.rc('xtick', direction='out', color='white')
-    plt.rc('ytick', direction='out', color='white')
-    plt.rc('patch', edgecolor='#E6E6E6')
+    # plt.rc('legend', facecolor='#666666EE', edgecolor='white', fontsize=16)
+    # plt.rc('grid', color='white', linestyle='solid')
+    # plt.rc('text', color='white')
+    # plt.rc('xtick', direction='out', color='white')
+    # plt.rc('ytick', direction='out', color='white')
+    # plt.rc('patch', edgecolor='#E6E6E6')
 
 # define function that allows to generate a number of sub plots in a single line with the given titles
 def prep_plots(titles, fig_size, fig_num=1):
